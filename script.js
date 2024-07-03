2c520d586bba45dec74d72b6c951a3c5ef9afd4c
shin <121587677+shinkoushi@users.noreply.github.com>
2024-07-03 13:58:03 +0700

    script.js

--- script.js
+++ script.js
@@ -9,35 +9,31 @@ function love(){
     }
     else{
         let random = Math.floor(Math.random()*100);
-        document.getElementById("print").innerHTML = " Cinta " + " " + name1 + " dan " + name2 + "" + " sebesar " + random + " % ";
-        move(random); // Move the call to move here
-        if (random >= 1 && random <= 30) {
-            document.getElementById("statement").innerHTML = "Hubungan kalian jelekkk bangett, mending jauh jauh deh";
-          } else if (random > 30 && random <= 50) {
-            document.getElementById("statement").innerHTML = "Hubungan kalian suram, gaada masa depan nya. Kamu jangan banyak berharap ya";
-          } else if (random > 50 && random <= 85) {
-            document.getElementById("statement").innerHTML = "Hubungan kalian bagus, tapi cuma cocok jadi temen deket aja";
-          } else if (random > 85 && random <= 100) {
-            document.getElementById("statement").innerHTML = "Hubungan kalian sangatt baguss, fiks jodoh !";
-          }
+        document.getElementById("print").innerHTML = "Tunggu di hitung dulu...";
+        move(random, function() {
+            document.getElementById("print").innerHTML = " Cinta " + " " + name1 + " dan " + name2 + "" + " sebesar " + random + " % ";
+            if (random >= 1 && random <= 30) {
+                document.getElementById("statement").innerHTML = "Hubungan kalian jelekkk bangett, mending jauh jauh deh";
+            } else if (random > 30 && random <= 50) {
+                document.getElementById("statement").innerHTML = "Hubungan kalian suram, gaada masa depan nya. Kamu jangan banyak berharap ya";
+            } else if (random > 50 && random <= 85) {
+                document.getElementById("statement").innerHTML = "Hubungan kalian bagus, tapi cuma cocok jadi temen deket aja";
+            } else if (random > 85 && random <= 100) {
+                document.getElementById("statement").innerHTML = "Hubungan kalian sangatt baguss, fiks jodoh !";
+            }
+        });
     }
-   
-}
-if (random) {
-    move(random);
 }
 
-
-function move(random) {
-  
+function move(random, callback) {
     const elem = document.querySelector(".progress-done");
     let width = 0;
     const main = setInterval(frame, 50);
 
-   
     function frame() {
         if (width >= random) {
             clearInterval(main);
+            callback(); 
         } else {
             width++;
             elem.style.width = width + "%";
